57bff1fa87df3ea3f4e3fabd14fe24901663a9f5
Kachamoric <TAHMIMRELEFT2002@gmail.com>
2025-03-01 18:52:10 +0600

    number

--- dom/dev.js
+++ dom/dev.js
@@ -3,9 +3,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     buttons.forEach(button => {
         button.addEventListener("click", function () {
-            alert("Challenge Completed!"); 
-            disabled = true;
-            classList.add("opacity-50", "cursor-not-allowed"); 
+            alert("Challenge Completed"); 
+            this.disabled = true;
+            this.classList.add("opacity-50", "cursor-not-allowed"); 
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.completed-btn').forEach(button => {
+        button.addEventListener('click', function() {
+            let completedTasks = document.getElementById('completed-tasks');
+            let taskAssigned = document.getElementById('task-assigned');
+
+            let completedCount = parseInt(completedTasks.innerText);
+            let assignedCount = parseInt(taskAssigned.innerText);
+
+            if (assignedCount > 0) {
+                completedTasks.innerText = completedCount + 1;
+                taskAssigned.innerText = assignedCount - 1;
+            }
         });
     });
 });
